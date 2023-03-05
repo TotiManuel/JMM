8a6f2800d7e628d42b054f84e9ef60a2b69bdc25
TotiManuel <manuel.mandaio@gmail.com>
2023-03-05 18:41:31 -0300

    Commit Automatico

--- Server/5Viernes/React/cvmandaio/src/App.js
+++ Server/5Viernes/React/cvmandaio/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Habilidades from './Componentes/Habilidades.jsx';
 
 function App() {
   return (
     <div className="App">
-      
+      <Habilidades />
     </div>
   );
 }
--- Server/5Viernes/React/cvmandaio/src/Componentes/Habilidades.jsx
+++ Server/5Viernes/React/cvmandaio/src/Componentes/Habilidades.jsx
@@ -6,10 +6,12 @@ function Habilidades() {
 			<img className='imagen-cv'
 			src={require('../Imagenes/web_logo.png')}
 			alt='Imagen CV' />
+			<div className='contenedor-texto'>
+			<p className='texto1'>Curriculum Vitae</p>
+			<p className='texto2'>Julian Mandaio</p>
+			<p className='texto3'>sdukfhsdulfhdusihfilusdhfauisdlfh</p>
+			</div>
 		</div>
 	);
 }
-
-
-<div className=''></div>
-<div className=''></div>
+export default Habilidades;
