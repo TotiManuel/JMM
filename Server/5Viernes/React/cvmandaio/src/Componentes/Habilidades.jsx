60f141b18bc1d332721d707e78ba842e0b9793c0
TotiManuel <manuel.mandaio@gmail.com>
2023-03-05 18:06:31 -0300

    Commit Automatico

---
+++ Server/5Viernes/React/cvmandaio/src/Componentes/Habilidades.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Habilidades() {
+	return(
+		<div className='contenedor-habilidades'>
+			<img className='imagen-cv'
+			src={require('../Imagenes/web_logo.png')}
+			alt='Imagen CV' />
+		</div>
+	);
+}
+
+
+<div className=''></div>
+<div className=''></div>
