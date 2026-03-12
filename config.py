6c768c454e46c4b2389357320d0812e9c333a0ff
TotiManuel <manuel.mandaio@gmail.com>
2026-03-12 05:25:43 -0300

    fix

--- config.py
+++ config.py
@@ -4,3 +4,8 @@ class Config:
     SECRET_KEY = "clave_secreta"
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_pre_ping": True,
+        "pool_recycle": 280
+    }
