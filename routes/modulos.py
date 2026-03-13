37d3f48000faedad0fe1381bb9c9fa1eff515ea3
TotiManuel <manuel.mandaio@gmail.com>
2026-03-13 01:55:59 -0300

    Update seccion clases

--- routes/modulos.py
+++ routes/modulos.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from utils.auth import login_required
-from models.models import Modulo, Nota, Archivo, Video, Enlace, Tarea
+from extensions import db
+from models.models import Clase, Modulo, Nota, Archivo, Video, Enlace, Tarea
 
 modulos = Blueprint("modulos", __name__)
 
@@ -34,8 +35,67 @@ def ver_seccion(codigo, seccion):
 
     modulo = Modulo.query.filter_by(codigo=codigo).first_or_404()
 
+    clases = []
+
+    if seccion == "clases":
+        clases = Clase.query.filter_by(modulo_id=modulo.id).all()
+
     return render_template(
         "seccion.html",
         modulo=modulo,
-        seccion=seccion
+        seccion=seccion,
+        clases=clases
     )
+    
+@modulos.route("/clase/crear/<codigo>", methods=["POST"])
+@login_required
+def crear_clase(codigo):
+
+    from flask import request, redirect
+
+    modulo = Modulo.query.filter_by(codigo=codigo).first_or_404()
+
+    titulo = request.form["titulo"]
+    contenido = request.form["contenido"]
+
+    nueva = Clase(
+        titulo=titulo,
+        contenido=contenido,
+        modulo_id=modulo.id
+    )
+
+    db.session.add(nueva)
+    db.session.commit()
+
+    return redirect(f"/modulo/{codigo}/clases")
+
+@modulos.route("/clase/editar/<int:id>", methods=["POST"])
+@login_required
+def editar_clase(id):
+
+    from flask import request, redirect
+
+    clase = Clase.query.get_or_404(id)
+
+    clase.titulo = request.form["titulo"]
+    clase.contenido = request.form["contenido"]
+
+    db.session.commit()
+
+    return redirect(f"/modulo/{clase.modulo.codigo}/clases")
+
+@modulos.route("/clase/eliminar/<int:id>")
+@login_required
+def eliminar_clase(id):
+
+    from flask import redirect
+
+    clase = Clase.query.get_or_404(id)
+    codigo = clase.modulo.codigo
+
+    db.session.delete(clase)
+    db.session.commit()
+
+    return redirect(f"/modulo/{codigo}/clases")
+
+
