238ece7d1d53fb341748e8710be63befb3dedd89
TotiManuel <manuel.mandaio@gmail.com>
2026-03-13 06:55:51 -0300

    remove nota.html

--- routes/notas.py
+++ routes/notas.py
@@ -1,19 +1,8 @@
-from flask import Blueprint, render_template, url_for, request, redirect
+from flask import Blueprint, url_for, request, redirect
 from models.models import Nota
 from extensions import db
 
 notas = Blueprint("notas", __name__)
-
-@notas.route("/nota/<int:id>")
-def ver_nota(id):
-
-    nota = Nota.query.get_or_404(id)
-
-    return render_template(
-        "nota.html",
-        nota=nota
-    )
-
 
 @notas.route("/nota/crear/<codigo>", methods=["POST"])
 def crear_nota(codigo):
@@ -30,7 +19,7 @@ def crear_nota(codigo):
     db.session.add(nota)
     db.session.commit()
 
-    return redirect(url_for("notas.ver_nota", id=nota.id))
+    return redirect(url_for("modulos.ver_modulo", codigo=codigo))
 
 
 @notas.route("/nota/editar/<int:id>", methods=["POST"])
@@ -43,7 +32,10 @@ def editar_nota(id):
 
     db.session.commit()
 
-    return redirect(url_for("notas.ver_nota", id=nota.id))
+    return redirect(url_for(
+        "modulos.ver_modulo",
+        codigo=nota.modulo_codigo
+    ))
 
 
 @notas.route("/nota/eliminar/<int:id>")
@@ -51,7 +43,12 @@ def eliminar_nota(id):
 
     nota = Nota.query.get_or_404(id)
 
+    codigo = nota.modulo_codigo
+
     db.session.delete(nota)
     db.session.commit()
 
-    return redirect("/")
+    return redirect(url_for(
+        "modulos.ver_modulo",
+        codigo=codigo
+    ))
