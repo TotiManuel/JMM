bbf51106062d4198f183f03761844d5ece5ad642
TotiManuel <manuel.mandaio@gmail.com>
2026-03-13 02:03:27 -0300

    Update model clase

--- models/models.py
+++ models/models.py
@@ -2,23 +2,20 @@ from extensions import db
 from datetime import datetime
 
 class Clase(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    titulo = db.Column(db.String(200), nullable=False)
-    descripcion = db.Column(db.String(300))
-    fecha = db.Column(db.DateTime, default=datetime.utcnow)
 
-    bloques = db.relationship('BloqueClase', backref='clase', cascade="all, delete")
-
-
-class BloqueClase(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
-    clase_id = db.Column(db.Integer, db.ForeignKey('clase.id'))
+    titulo = db.Column(db.String(200))
 
-    tipo = db.Column(db.String(50))
     contenido = db.Column(db.Text)
 
-    orden = db.Column(db.Integer)
+    modulo_id = db.Column(
+        db.Integer,
+        db.ForeignKey("modulos.id"),
+        nullable=False
+    )
+
+    modulo = db.relationship("Modulo", backref="clases")
 
 class Modulo(db.Model):
 
