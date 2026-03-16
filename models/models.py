c1b6405546dd281d9f4dd72649f3d9b42129961e
TotiManuel <manuel.mandaio@gmail.com>
2026-03-16 03:55:35 -0300

    fix model

--- models/models.py
+++ models/models.py
@@ -67,7 +67,7 @@ class Nota(db.Model):
     def __repr__(self):
         return f"<Nota {self.titulo}>"
 class Archivo(db.Model):
-
+    __tablename__ = "archivos"
     id = db.Column(db.Integer, primary_key=True)
 
     nombre = db.Column(db.String(200))
